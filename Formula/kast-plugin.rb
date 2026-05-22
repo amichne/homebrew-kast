0c2b3abc6a5fc37ee0a6606a30532101f98d99c4
Austin Michne <austin@michne.com>
2026-05-22 19:17:34 -0400

    fix: order plugin formula version before url

--- Formula/kast-plugin.rb
+++ Formula/kast-plugin.rb
@@ -1,8 +1,8 @@
 class KastPlugin < Formula
   desc "IntelliJ IDEA plugin bundle for Kast Kotlin analysis"
   homepage "https://github.com/amichne/kast"
-  url "https://github.com/amichne/kast/releases/download/v#{version}/kast-intellij-v#{version}.zip"
   version "0.7.14"
+  url "https://github.com/amichne/kast/releases/download/v#{version}/kast-intellij-v#{version}.zip"
   sha256 "5b1e3f9c35feaa05956c4af097f9c95bbcd45bd58531676bf1c915c0150cae0f"
   license "Apache-2.0"
 
